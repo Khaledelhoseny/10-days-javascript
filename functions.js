787f582c4a85a155e3dc16f72362b4a8455e53a7
JS-Training <js-training@Mohammeds-iMac.local>
2022-08-03 16:56:04 +0200

    added function js file

---
+++ functions.js
@@ -0,0 +1,19 @@
+
+/*
+ * Create the function factorial here
+ */
+function factorial(n){
+
+    for(let i = n-1 ; i>=1 ; i-- ){
+    
+        n = n*i
+    }
+    
+    return n 
+    
+    }
+    
+    
+    
+    
+    
