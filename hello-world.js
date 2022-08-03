3d5068c7416b6568ac1cdb6f99908d3aca22d652
JS-Training <js-training@Mohammeds-iMac.local>
2022-08-03 11:50:36 +0200

    added hello-world

---
+++ hello-world.js
@@ -0,0 +1,30 @@
+
+
+function vowelsAndConsonants(s) {
+let x = 'aouei'
+let vowelsArr = []
+let constantsArr = []
+
+
+    for (let i = 0 ; i<s.length ; i++) {
+
+
+        if (x.includes(s[i]) ) {
+
+          vowelsArr.push(s[i])
+            
+        }else{
+
+          constantsArr.push(s[i])
+
+        }
+
+        
+    }
+    vowelsArr.concat(constantsArr).forEach(char => console.log(char))
+
+    
+
+}
+
+
