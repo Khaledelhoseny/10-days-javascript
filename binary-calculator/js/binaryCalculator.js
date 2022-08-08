640ba5a9592f23f70b2d6e2bbd8452745ea3fc9d
JS-Training <js-training@Mohammeds-iMac.local>
2022-08-08 17:08:36 +0200

    changes binary-calculator project

--- binary-calculator/js/binaryCalculator.js
+++ binary-calculator/js/binaryCalculator.js
@@ -1,63 +1,16 @@
-// let button0 = document.querySelector("#btn0") 
-// let button1 = document.querySelector("#btn1")
-// let buttonSum = document.querySelector("#btnSum")
-// let buttonEqual = document.querySelector("#btnEql")
-// let result = document.querySelector("#res")
 
-// button0.addEventListener("click" , showButton0 )
-
-// function showButton0(){
-  
-    
-//   result.innerText+=button0.innerText
-
-//   let x = result.innerText
-//   console.log(x)
-
-  
-// }
-
-
-// button1.addEventListener("click" , showButton1 )
-// function showButton1(){
-   
-//     result.innerText +=  button1.innerText 
-//     let y = result.innerText
-//     console.log(y)
-
-    
-// }
-
-// buttonSum.addEventListener("click" , showbuttonSum )
-// function showbuttonSum(){
-   
-//     result.innerText +=  buttonSum.innerText 
-// }
-
-
-
-
-
-// buttonEqual.addEventListener("click" ,equalFunc)
-
-
-// function equalFunc(){
-
-//     let deci1 = parseInt(x, 2); //decimal number
-//     let deci2 = parseInt(y, 2); //decimal number
-//     console.log(deci1)
-//     // let resultDecimals = deci1 + deci2 
-//     // console.log(resultDecimals)
-
-// }
 
 
 let button0 = document.querySelector("#btn0") 
 let button1 = document.querySelector("#btn1")
 let buttonSum = document.querySelector("#btnSum")
 let buttonEqual = document.querySelector("#btnEql")
-let result = document.querySelector("#res")
+let buttonSub = document.querySelector("#btnSub")
+let buttonMul = document.querySelector("#btnMul")
+let buttonDiv = document.querySelector("#btnDiv")
+let buttonClear = document.querySelector("#btnClr")
 
+let result = document.querySelector("#res")
 
 
 button0.addEventListener("click" , showButton0 )
@@ -83,14 +36,38 @@ function showButton1(){
 }
 
 
-// buttonSum.addEventListener("click" , showbuttonSum )
-// function showbuttonSum(){
-   
-//     result.innerText +=  buttonSum.innerText 
-// }
 
 
-// result.innerText = result0 + result1
+buttonSum.addEventListener("click" , sumFunc )
+function sumFunc(){
+  result.innerText+=buttonSum.innerText
+}
+
+
+buttonSub.addEventListener("click" , subFunc )
+function subFunc(){
+  result.innerText+=buttonSub.innerText
+}
+
+buttonMul.addEventListener("click" , mulFunc )
+function mulFunc(){
+  result.innerText+=buttonMul.innerText
+}
+
+buttonDiv.addEventListener("click" , divFunc )
+function divFunc(){
+  result.innerText+=buttonDiv.innerText
+}
+
+buttonClear.addEventListener("click" , clearFunc )
+function clearFunc(){
+  console.log("clearr")
+
+  result.innerText = result.innerText.substring(0, result.innerText.length - 1);
+
+}
+
+
 
 
 buttonEqual.addEventListener("click" ,equalFunc)
@@ -98,15 +75,56 @@ buttonEqual.addEventListener("click" ,equalFunc)
 function equalFunc(){
 
   
-  console.log(result.innerText) 
-  let deci = parseInt(result.innerText, 2);
-  console.log(deci)
-  
-    // let deci1 = parseInt(result, 2); //decimal number
-    //  //decimal number
-    // console.log(deci1)
-        
-    // let resultDecimals = deci1 + deci2 
-    // console.log(resultDecimals)
 
+  if(result.innerText.includes("+")){
+  let operand1 =  result.innerText.slice(result.innerText.indexOf("+")+1)
+  let operand2 =  result.innerText.slice(0,result.innerText.indexOf("+"))
+  let deci1 = parseInt(operand1, 2)
+  let deci2 = parseInt(operand2, 2)
+    clear()
+    result.innerText =  Number(deci1+deci2).toString(2)
+    
+  }else if(result.innerText.includes("-")){
+  let operand2 =  result.innerText.slice(result.innerText.indexOf("-")+1)
+  let operand1 =  result.innerText.slice(0,result.innerText.indexOf("-"))
+  let deci1 = parseInt(operand1, 2)
+  let deci2 = parseInt(operand2, 2)
+    clear()
+    result.innerText =  Number(deci1-deci2).toString(2)
+  }else if(result.innerText.includes("*")){
+
+    let operand1 =  result.innerText.slice(0,result.innerText.indexOf("*"))
+    let operand2 =  result.innerText.slice(result.innerText.indexOf("*")+1)
+    let deci1 = parseInt(operand1, 2)
+    let deci2 = parseInt(operand2, 2)
+      clear()
+      result.innerText =  Number(deci1*deci2).toString(2)
+  }else if(result.innerText.includes("/")){
+
+    let operand1 =  result.innerText.slice(0,result.innerText.indexOf("/"))
+    let operand2 =  result.innerText.slice(result.innerText.indexOf("/")+1)
+    let deci1 = parseInt(operand1, 2)
+    let deci2 = parseInt(operand2, 2)
+      clear()
+      result.innerText =  Number(deci1/deci2).toString(2)
+  }
+
+
+function clear(){
+  result.innerText = ''
 }
+  
+}
+
+
+
+
+
+
+
+ 
+
+
+
+
+
