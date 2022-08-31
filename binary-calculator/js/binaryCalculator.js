7a0136479b9052f0e117370f43da5b9926dee378
JS-Training <js-training@Mohammeds-iMac.local>
2022-08-31 10:30:13 +0200

    changes

--- binary-calculator/js/binaryCalculator.js
+++ binary-calculator/js/binaryCalculator.js
@@ -1,4 +1,4 @@
-let button0 = document.querySelector("#btn0") 
+let button0 = document.querySelector("#btn0")
 let button1 = document.querySelector("#btn1")
 let buttonSum = document.querySelector("#btnSum")
 let buttonEqual = document.querySelector("#btnEql")
@@ -6,120 +6,188 @@ let buttonSub = document.querySelector("#btnSub")
 let buttonMul = document.querySelector("#btnMul")
 let buttonDiv = document.querySelector("#btnDiv")
 let buttonClear = document.querySelector("#btnClr")
-
 let result = document.querySelector("#res")
 
 
-button0.addEventListener("click" , showButton0 )
+button0.addEventListener("click", showButton0)
 
 
-function showButton0(){
-  
-  result.innerText+=button0.innerText
+function showButton0() {
+  if (bolean) {
+    clearFunc()
+    bolean = false
+  }
+  result.innerText += button0.innerText
+  console.log(result.innerText)
+
+}
+button1.addEventListener("click", showButton1)
+
+function showButton1() {
+  if (bolean) {
+    clearFunc()
+    bolean = false
+  }
+  result.innerText += button1.innerText
 
   console.log(result.innerText)
+
+}
+
+
+
+
+// buttonSum.addEventListener("click", sumFunc)
+// function sumFunc() {
+//   let lastIndex = result.innerText[result.innerText.length-1]  
+//   console.log(lastIndex)
+
+//   if(lastIndex==='0'|lastIndex==='1'){
+//     result.innerText += buttonSum.innerText
+//   }else if(lastIndex==='+'){  
+//     console.log("dont")
+//   }else if(lastIndex==='-'|lastIndex==='*'|lastIndex==='/'){
+//     result.innerText =  result.innerText.slice(0 , result.innerText.indexOf(lastIndex))   
+//     result.innerText+=buttonSum.innerText
+//   }
+
+
+// }
+
+
+
+buttonSum.addEventListener("click", sumFunc)
+function sumFunc() {
   
-}
-button1.addEventListener("click" , showButton1 )
-
-
-
-function showButton1(){
+  operand=result.innerText
  
-  result.innerText+=button1.innerText
- 
-    console.log(result.innerText)
-  
+  let regex = /[\+\-\/\*]/ig   ; 
+  if(regex.test(operand)){
+    operand = operand.replace(regex,"+")
+  }else{
+    operand+="+"
+    
+
+  } 
+  result.innerText = operand
 }
 
 
 
 
-buttonSum.addEventListener("click" , sumFunc )
-function sumFunc(){
-  result.innerText+=buttonSum.innerText
+buttonSub.addEventListener("click", subFunc)
+function subFunc() {
+
+  operand=result.innerText
+  let regex = /[\+\-\/\*]/ig ; 
+  if(regex.test(operand)){
+    operand = operand.replace(regex,"-")
+  }else{
+    operand+="-"
+    
+  } 
+  result.innerText = operand
+
+
+
 }
 
 
-buttonSub.addEventListener("click" , subFunc )
-function subFunc(){
-  result.innerText+=buttonSub.innerText
+buttonMul.addEventListener("click", mulFunc)
+function mulFunc() {
+
+  operand=result.innerText
+  let regex = /[\+\-\/\*]/ig ; 
+  if(regex.test(operand)){
+    operand = operand.replace(regex,"*")
+  }else{
+    operand+="*"
+    
+  } 
+  result.innerText = operand
+
 }
 
-buttonMul.addEventListener("click" , mulFunc )
-function mulFunc(){
-  result.innerText+=buttonMul.innerText
+buttonDiv.addEventListener("click", divFunc)
+function divFunc() {
+
+  operand=result.innerText
+  let regex = /[\+\-\/\*]/ig ; 
+  if(regex.test(operand)){
+    operand = operand.replace(regex,"/")
+  }else{
+    operand+="/"
+    
+  } 
+  result.innerText = operand
+
 }
 
-buttonDiv.addEventListener("click" , divFunc )
-function divFunc(){
-  result.innerText+=buttonDiv.innerText
-}
 
-buttonClear.addEventListener("click" , clearFunc )
-function clearFunc(){
+
+
+buttonClear.addEventListener("click", clearFunc)
+function clearFunc() {
   console.log("clearr")
 
-  result.innerText = result.innerText.substring(0, result.innerText.length - 1);
+  // result.innerText = result.innerText.substring(result.innerText.length);
+  result.innerText = ''
 
 }
 
 
 
 
-buttonEqual.addEventListener("click" ,equalFunc)
+buttonEqual.addEventListener("click", equalFunc)
+let bolean
+function equalFunc() {
 
-function equalFunc(){
-
-  
-
-  if(result.innerText.includes("+")){
-  let operand1 =  result.innerText.slice(result.innerText.indexOf("+")+1)
-  let operand2 =  result.innerText.slice(0,result.innerText.indexOf("+"))
-  let deci1 = parseInt(operand1, 2)
-  let deci2 = parseInt(operand2, 2)
-    clear()
-    result.innerText =  Number(deci1+deci2).toString(2)
-    
-  }else if(result.innerText.includes("-")){
-  let operand2 =  result.innerText.slice(result.innerText.indexOf("-")+1)
-  let operand1 =  result.innerText.slice(0,result.innerText.indexOf("-"))
-  let deci1 = parseInt(operand1, 2)
-  let deci2 = parseInt(operand2, 2)
-    clear()
-    result.innerText =  Number(deci1-deci2).toString(2)
-  }else if(result.innerText.includes("*")){
-
-    let operand1 =  result.innerText.slice(0,result.innerText.indexOf("*"))
-    let operand2 =  result.innerText.slice(result.innerText.indexOf("*")+1)
+  if (result.innerText.includes("+")) {
+    let operand1 = result.innerText.slice(result.innerText.indexOf("+") + 1)
+    let operand2 = result.innerText.slice(0, result.innerText.indexOf("+"))
+    console.log(operand1)
     let deci1 = parseInt(operand1, 2)
     let deci2 = parseInt(operand2, 2)
-      clear()
-      result.innerText =  Number(deci1*deci2).toString(2)
-  }else if(result.innerText.includes("/")){
+    clear()
+    result.innerText = Number(deci1 + deci2).toString(2)
+    bolean = true
 
-    let operand1 =  result.innerText.slice(0,result.innerText.indexOf("/"))
-    let operand2 =  result.innerText.slice(result.innerText.indexOf("/")+1)
+  } else if (result.innerText.includes("-")) {
+    let operand2 = result.innerText.slice(result.innerText.indexOf("-") + 1)
+    let operand1 = result.innerText.slice(0, result.innerText.indexOf("-"))
     let deci1 = parseInt(operand1, 2)
     let deci2 = parseInt(operand2, 2)
-      clear()
-      result.innerText =  Number(deci1/deci2).toString(2)
+    clear()
+    result.innerText = Number(deci1 - deci2).toString(2)
+  } else if (result.innerText.includes("*")) {
+
+    let operand1 = result.innerText.slice(0, result.innerText.indexOf("*"))
+    let operand2 = result.innerText.slice(result.innerText.indexOf("*") + 1)
+    let deci1 = parseInt(operand1, 2)
+    let deci2 = parseInt(operand2, 2)
+    clear()
+    result.innerText = Number(deci1 * deci2).toString(2)
+  } else if (result.innerText.includes("/")) {
+
+    let operand1 = result.innerText.slice(0, result.innerText.indexOf("/"))
+    let operand2 = result.innerText.slice(result.innerText.indexOf("/") + 1)
+    let deci1 = parseInt(operand1, 2)
+    let deci2 = parseInt(operand2, 2)
+    clear()
+    result.innerText = Number(deci1 / deci2).toString(2)
   }
 
 
-function clear(){
-  result.innerText = ''
+  function clear() {
+    result.innerText = ''
+  }
+
 }
-  
-}
 
 
 
 
 
-
-
- 
 
 
 
--- bitwise-operators.js
+++ bitwise-operators.js
@@ -1,119 +1,14 @@
-// function getMaxLessThanK(n,k){
+function getMaxLessThanK(n, k) {
+    let value = 0;
+    for (let y = 1; y < n; y++) {
+      for (let j = y + 1; j <= n; j++) {
+        let result = y & j;
+        if (result < k && result > value) {
+          value = result;
+        }
+      }
+    }
+    return value;
+  }
 
-
-//     let resulArray = []
-//     for(let i = 0 ; i<s.length ; i++){
-    
-//         let newS =  s.slice(s[i])
-        
-//         for(let j = 0 ; j<newS.length ; j++){
-    
-//             let result = s[i] & newS[j]        
-//             resulArray.push(result)
-            
-         
-//          }
-         
-         
-//     }
-    
-//     console.log(resulArray)
-    
-//     for(let i = 0 ; i<resulArray.length ; i++){
-    
-//         if (resulArray[i]<k ){
-//             console.log(resulArray[i] )
-//         }
-        
-//     }
-    
-//     }
-    
-
-
-// let s = [1,2,3,4,5,6,7,8]
-
-
-// let resulArray = []
-// for(let i = 0 ; i<s.length ; i++){
-
-//     let newS =  s.slice(s[i])
-    
-//     for(let j = 0 ; j<newS.length ; j++){
-
-//         let result = s[i] & newS[j]        
-//         resulArray.push(result)
-        
-     
-//      }
-     
-// }
-
-// // console.log(resulArray)
-// let maxArray = []
-
-// for(let i = 0 ; i<resulArray.length ; i++){
-
-//     if (resulArray[i] < 5 ){
-//         maxArray.push(resulArray[i])
-        
-//     }
-    
-// }
-// console.log( Math.max(...maxArray))
-
-
-
-
-
-// s == n , k = 5
-
-
-
-let finalResult = 0 
-let secondHeighst = 0
-function getMaxLessThanK(n,k){
-
-let input_array =  Array.from(Array(n+1).keys()).slice(1)
-
-for(let i = 0 ; i<input_array.length ; i++){
-
-    let newS =  input_array.slice(input_array[i])
-    
-    for(let j = 1 ; j<newS.length ; j++){
-        
-       let andResult = input_array[i] & newS[j]   //5
-      
-      //  if(finalResult<k){ 
-      //   finalResult = andResult
-      //   secondHeighst = finalResult
-           
-      //  }
-       
-     }
-     
-}
-console.log(andResult)
-
-
-
-
-// let maxArray = []
-// for(let i = 0 ; i < resulArray.length ; i++){  //resultArray = [0]
-
-//     if (resulArray[i] < k  ){
-//         maxArray.push(resulArray[i])
-        
-//     }
-    
-// }
-// console.log(maxArray)
-// resulArray =[]
-// return Math.max(...maxArray)
-
-}
-
-
-
-
-console.log("kjaled")
+  
